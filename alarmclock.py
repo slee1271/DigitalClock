aa9b9fba239beaa4bec2bbc021091ab1768b7435
Shawn Lee <slee1271@mtroyal.ca>
2024-11-02 23:17:37 -0600

    need to finish update time

--- alarmclock.py
+++ alarmclock.py
@@ -1,1 +1,32 @@
-print("hello world!")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QTimer, QTime, Qt
+
+class DigitalClock(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.time_label = QLabel("10:10:00",self)
+        self.timer = QTimer(self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Digital Clock") 
+        self.setGeometry(600,400,300,100)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+        self.setLayout(vbox)
+
+        self.time_label.setAlignment(Qt.AlignCenter)
+        self.time_label.setStyleSheet("font-size: 150px;"
+                                      "font-family: Arial;"
+                                      "color: rgb(0, 255, 0);")
+        self.setStyleSheet("background-color: black;")
+    def update_time(self):
+        current_time = 
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    clock = DigitalClock()
+    clock.show()
+    sys.exit(app.exec_())
